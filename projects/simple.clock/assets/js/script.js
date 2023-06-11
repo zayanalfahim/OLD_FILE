9bc3449ef18df15810db07f7a929df29837ab8f6
ZAYAN AL FAHIM <121105386+zayanalfahim@users.noreply.github.com>
2023-06-11 15:12:42 +0600

    Add files via upload

--- projects/simple.clock/assets/js/script.js
+++ projects/simple.clock/assets/js/script.js
@@ -1,108 +1,76 @@
-/*==================== CLOCK ====================*/
+/*=============== CLOCK ===============*/
 const hour = document.getElementById('clock-hour'),
-      minutes = document.getElementById('clock-minutes'),
-      seconds = document.getElementById('clock-seconds')
+      minutes = document.getElementById('clock-minutes')
 
 const clock = () =>{
-    let date = new Date()
+   // We get the Date object
+   let date = new Date()
 
-    let hh = date.getHours() * 30,
-        mm = date.getMinutes() * 6,
-        ss = date.getSeconds() * 6
-        
-    // We add a rotation to the elements
-    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
-    minutes.style.transform = `rotateZ(${mm}deg)`
-    seconds.style.transform = `rotateZ(${ss}deg)`
+   // We get the hours and minutes 
+   // (current time) / 12(hours) * 360(deg circle)
+   // (Current minute) / 60(minutes) * 360(deg circle)
+   let hh = date.getHours() / 12 * 360,
+       mm = date.getMinutes() / 60 * 360
+
+   // We add a rotation to the elements
+   hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+   minutes.style.transform = `rotateZ(${mm}deg)`
 }
-setInterval(clock, 1000) // 1000 = 1s
+setInterval(clock, 1000) // (Updates every 1s) 1000 = 1s 
 
-/*==================== CLOCK & DATE TEXT ====================*/
-const textHour = document.getElementById('text-hour'),
-      textMinutes = document.getElementById('text-minutes'),
-      textAmPm = document.getElementById('text-ampm'),
-    //   dateWeek = document.getElementById('date-day-week'),
-      dateDay = document.getElementById('date-day'),
+/*=============== TIME AND DATE TEXT ===============*/
+const dateDayWeek = document.getElementById('date-day-week'),
       dateMonth = document.getElementById('date-month'),
-      dateYear = document.getElementById('date-year')
+      dateDay = document.getElementById('date-day'),
+      dateYear = document.getElementById('date-year'),
+      textHour = document.getElementById('text-hour'),
+      textMinutes = document.getElementById('text-minutes'),
+      textAmPm = document.getElementById('text-ampm')
 
 const clockText = () =>{
-    let date = new Date()
+   // We get the Date object
+   let date = new Date()
 
-    let hh = date.getHours(),
-        ampm,
-        mm = date.getMinutes(),
-        day = date.getDate(),
-        // dayweek = date.getDay(),
-        month = date.getMonth(),
-        year = date.getFullYear()
+   // We get the time and date
+   let dayWeek = date.getDay(),
+       month = date.getMonth(),
+       day = date.getDate(),
+       year = date.getFullYear(),
+       hh = date.getHours(),
+       mm = date.getMinutes(),
+       ampm
 
-    // We change the hours from 24 to 12 hours and establish whether it is AM or PM
-    if(hh >= 12){
-        hh = hh - 12
-        ampm = 'PM'
-    }else{
-        ampm = 'AM'
-    }
+   // We get the days of the week and the months. (First day of the week Sunday)
+   let daysWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+   let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-    // We detect when it's 0 AM and transform to 12 AM
-    if(hh == 0){hh = 12}
+   // We add the corresponding dates
+   dateDayWeek.innerHTML = `${daysWeek[dayWeek]}`
+   dateMonth.innerHTML = `${months[month]}`
+   dateDay.innerHTML = `${day}, `
+   dateYear.innerHTML = year
 
-    // Show a zero before hours
-    if(hh < 10){hh = `0${hh}`}
+   // If hours is greater than 12 (afternoon), we subtract -12, so that it starts at 1 (afternoon)
+   if(hh >= 12){
+      hh = hh - 12
+      ampm = 'PM'
+   } else{
+      ampm = 'AM'
+   }
 
-    // Show time
-    textHour.innerHTML = `${hh}:`
-    
-    // Show a zero before the minutes
-    if(mm < 10){mm = `0${mm}`}
-    
-    // Show minutes
-    textMinutes.innerHTML = mm
+   textAmPm.innerHTML = ampm
 
-    // Show am or pm
-    textAmPm.innerHTML = ampm
+   // When it is 0 hours (early morning), we tell it to change to 12 hours
+   if(hh == 0){hh = 12}
 
-    // If you want to show the name of the day of the week
-    // let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+   // If hours is less than 10, add a 0 (01,02,03...09)
+   if(hh < 10){hh = `0${hh}`}
 
-    // We get the months of the year and show it
-    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+   textHour.innerHTML = `${hh}:`
 
-    // We show the day, the month and the year
-    dateDay.innerHTML = day
-    // dateWeek.innerHTML = `${week[dayweek]}`
-    dateMonth.innerHTML = `${months[month]},`
-    dateYear.innerHTML = year
+   // If minutes is less than 10, add a 0 (01,02,03...09)
+   if(mm < 10){mm = `0${mm}`}
+
+   textMinutes.innerHTML = mm
 }
-setInterval(clockText, 1000) // 1000 = 1s
-
-/*==================== DARK/LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bxs-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+setInterval(clockText, 1000) // (Updates every 1s) 1000 = 1s
